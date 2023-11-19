b1028a13f8617b7078f28027efd501010393d7d8
chri42mp <99641832+chri42mp@users.noreply.github.com>
2023-11-19 10:08:47 +0100

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCYfwR5BUFZGwfEQavsDgXC56GIVUp1xNs",
+  authDomain: "klinik-straarup.firebaseapp.com",
+  projectId: "klinik-straarup",
+  storageBucket: "klinik-straarup.appspot.com",
+  messagingSenderId: "1088852793249",
+  appId: "1:1088852793249:web:188f5ffd2dc3d81ce0dde0",
+  measurementId: "G-5FMRDQ670D",
+};
+
+// Initialize Firebase
+export const app = firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth(app);
+export const database = firebase.firestore(app);
