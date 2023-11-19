751894313ea1b27ef95ed347f202c53b7b4a73be
chri42mp <99641832+chri42mp@users.noreply.github.com>
2023-11-19 10:08:12 +0100

    svg icons setup

---
+++ src/assets/icons/MenuIcon.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const MenuIcon = () => (
+  <svg
+    width="50"
+    height="50"
+    viewBox="0 0 50 50"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M5 9H45"
+      stroke="#161616"
+      stroke-width="3"
+      stroke-linecap="round"
+    />
+    <path
+      d="M5 25H45"
+      stroke="#161616"
+      stroke-width="3"
+      stroke-linecap="round"
+    />
+    <path
+      d="M5 41H45"
+      stroke="#161616"
+      stroke-width="3"
+      stroke-linecap="round"
+    />
+  </svg>
+);
+
+export default MenuIcon;
