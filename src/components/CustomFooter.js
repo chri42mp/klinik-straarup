f46754fc4a6057ebfa472eaf07d8317effa79289
Kamilla <kamillakkngmail.com>
2023-11-23 21:32:46 +0100

    started footer

---
+++ src/components/CustomFooter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CustomFooter = () => {
+  return (
+    <footer>
+      <div></div>
+      <div>
+        <ul></ul>
+      </div>
+      <div></div>
+      <div></div>
+    </footer>
+  );
+};
+
+export default CustomFooter;
