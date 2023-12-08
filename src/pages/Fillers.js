4c2f3365ca9d907cb7237ba8c3e97a60c416ea7c
Kamilla <kamillakkngmail.com>
2023-12-08 12:59:23 +0100

    added single banner responsive

--- src/pages/Fillers.js
+++ src/pages/Fillers.js
@@ -1,11 +1,24 @@
-import React from "react";
-import Navigation from "../components/Navigation";
+import React from 'react';
+import Navigation from '../components/Navigation';
+import './Fillers.scss';
+import SingleBanner from '../components/SingleBanner';
 
 export default function Fillers() {
   return (
     <>
       <Navigation />
-      <h1>Fillers</h1>
+      <div>
+        <SingleBanner
+          title='Getting skin ready'
+          subtitle='subheader'
+          text='En sund hud starter med det grundlæggende. Hjælp din hud tilbage til en sund, afbalancerede tilstand for en klar teint, uanset hudtype.'
+          imageUrl='/webshop-images/2018-kit-aggressive-anti-aging-program.webp'
+          alt='Calming toner'
+          buttonText='text'
+          onButtonClick={() => console.log('Button clicked!')}
+          reverse={false}
+        />
+      </div>
     </>
   );
 }
