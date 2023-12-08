aee2d168bd544991979f237b2abaa2aa3adca93a
Kamilla <kamillakkngmail.com>
2023-12-08 10:45:04 +0100

    added contactform, not setup or designed

---
+++ src/components/ContactForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ContactForm.scss';
+
+const ContactForm = () => {
+  return (
+    <div className='contact-form'>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default ContactForm;
