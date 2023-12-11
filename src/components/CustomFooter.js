5763a1ed0831fbe0bbf5ba2eee69211fb793a6b8
chri42mp <99641832+chri42mp@users.noreply.github.com>
2023-12-11 21:24:18 +0100

    wip

--- src/components/CustomFooter.js
+++ src/components/CustomFooter.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './CustomFooter.scss';
-import KsLogoGold from '../assets/logos/KsLogoGold';
-import FacebookIcon from '../assets/icons/FacebookIcon';
-import InstagramIcon from '../assets/icons/InstagramIcon';
-import TrustpilotStar from '../assets/icons/TrustpilotStar';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./CustomFooter.scss";
+import FacebookIcon from "../assets/icons/FacebookIcon";
+import InstagramIcon from "../assets/icons/InstagramIcon";
+import TrustpilotStar from "../assets/icons/TrustpilotStar";
 
 const CustomFooter = () => {
   return (
-    <footer className='custom-footer'>
-      <div className='top-div-footer'>
+    <footer className="custom-footer">
+      <div className="top-div-footer">
         <div
-          className='trustpilot'
-          data-locale='da-DK'
-          data-template-id='56278e9abfbbba0bdcd568bc'
-          data-businessunit-id='650d67597a3cc2a49edb1e74'
-          data-style-height='52px'
-          data-style-width='100%'
+          className="trustpilot"
+          data-locale="da-DK"
+          data-template-id="56278e9abfbbba0bdcd568bc"
+          data-businessunit-id="650d67597a3cc2a49edb1e74"
+          data-style-height="52px"
+          data-style-width="100%"
         >
-          <Link to='https://dk.trustpilot.com/review/klinikstraarup.dk'>
+          <Link to="https://dk.trustpilot.com/review/klinikstraarup.dk">
             <h4>Trustpilot</h4>
           </Link>
-          <div className='trustpilot-star'>
+          <div className="trustpilot-star">
             <TrustpilotStar />
             <TrustpilotStar />
             <TrustpilotStar />
@@ -30,26 +29,26 @@ const CustomFooter = () => {
           </div>
           <p>Rated 4.0/5.0</p>
         </div>
-        <div className='footer-contact'>
+        <div className="footer-contact">
           <h4>Kontakt</h4>
           <h5>E-mail</h5>
 
-          <Link to='mailto:info@klinikstraarup.dk'>info@klinikstraarup.dk</Link>
+          <Link to="mailto:info@klinikstraarup.dk">info@klinikstraarup.dk</Link>
           <h5>Telefon nr.</h5>
-          <Link to='tel:+4540562064'>+4540562064</Link>
+          <Link to="tel:+4540562064">+4540562064</Link>
           <h5>SoMe</h5>
-          <div className='some-icons'>
-            <Link to='https://www.instagram.com/klinikstraarup/'>
+          <div className="some-icons">
+            <Link to="https://www.instagram.com/klinikstraarup/">
               <InstagramIcon />
             </Link>
-            <Link to='https://www.facebook.com/klinikstraarup'>
+            <Link to="https://www.facebook.com/klinikstraarup">
               <FacebookIcon />
             </Link>
           </div>
         </div>
-        <div className='footer-address'>
+        <div className="footer-address">
           <h4>Adresse</h4>
-          <Link to='https://www.google.com/maps/search/?api=1&query=%22Klinik%20Straarup%2C%20Br%C3%B8ndbyvestervej%2023%20st%20th%20%2C%202600%20Glostrup%2C%20Denmark%22'>
+          <Link to="https://www.google.com/maps/search/?api=1&query=%22Klinik%20Straarup%2C%20Br%C3%B8ndbyvestervej%2023%20st%20th%20%2C%202600%20Glostrup%2C%20Denmark%22">
             <p>Klinik Straarup</p>
             <p>Brøndbyvestervej 23 st th</p>
             <p>2600 Glostrup </p>
@@ -57,15 +56,17 @@ const CustomFooter = () => {
           </Link>
         </div>
       </div>
-      <div className='bottom-div-footer'>
-        <div className='bottom-div-links'>
+      <div className="bottom-div-footer">
+        <div className="bottom-div-links">
           <p>Cookiepolitik</p>
           <p>Privatlivspolitik</p>
           <p>Sitemap</p>
         </div>
-        <div className='footer-logo'>
-          <KsLogoGold className='ks-logo-gold' />
-        </div>
+        <div className="footer-logo"></div>
+        <Link to="/">
+          <span className="visually-hidden">Gå til startsiden</span>
+          <img src="/KS-logo_gold.png" alt="klinik straarup logo" />
+        </Link>
       </div>
     </footer>
   );
