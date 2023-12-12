aaf493207674fef69bbb0bdc0fa353c497ec56f4
chri42mp <99641832+chri42mp@users.noreply.github.com>
2023-12-12 13:05:50 +0100

    local storage setup to basket

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -1,16 +1,36 @@
 import React, { useEffect, useState } from "react";
 import Navigation from "../components/Navigation";
 import { database } from "../firebase";
+import "./Basket.scss";
+import { PrimaryButton, SecondaryButton } from "../components/Buttons";
+import TrashIcon from "../assets/icons/TrashIcon";
+import { Link } from "react-router-dom";
+import ArrowLeftIcon from "../assets/icons/ArrowLeftIcon";
 
 export default function Basket() {
-  let localStorageInfo = localStorage.getItem("basket");
+  let [localStorageInfo, setLocalStorageInfo] = useState();
   const [empty, setEmpty] = useState(true);
   const [basket, setBasket] = useState([]);
   const [products, setproducts] = useState([]);
   const [totalprice, setTotalprice] = useState(0);
 
   useEffect(() => {
+    const newTotalPrice = basket.reduce((total, item) => {
+      return total + parseInt(item.qauntity) * parseFloat(item.productPrice);
+    }, 0);
+
+    setTotalprice(newTotalPrice);
+  }, [basket]);
+
+  useEffect(() => {
+    setLocalStorageInfo(localStorage.getItem("basket"));
+
     if (localStorageInfo) {
+      if (JSON.parse(localStorageInfo).length === 0) {
+        setEmpty(true);
+        setBasket([]);
+        return;
+      }
       setEmpty(false);
       setBasket(JSON.parse(localStorageInfo));
 
@@ -23,47 +43,130 @@ export default function Basket() {
     } else {
       setEmpty(true);
     }
-  }, [localStorageInfo]);
+  }, [basket.length, localStorageInfo]);
+
+  console.log(localStorageInfo);
+
+  function removeItem(e) {
+    const newBasket = basket.filter((item) => item.product !== e);
+
+    setBasket(newBasket);
+
+    localStorage.setItem("basket", JSON.stringify(newBasket));
+  }
+
+  function handleChangeInput(e, id) {
+    console.log(e.target.value, id);
+    const newValue = parseInt(e.target.value);
+
+    setBasket(
+      basket.map((item) => {
+        if (item.product === id) {
+          return {
+            ...item,
+            qauntity:
+              !isNaN(newValue) && newValue >= 1 ? newValue : item.qauntity,
+          };
+        }
+        return item;
+      })
+    );
+
+    localStorage.setItem("basket", JSON.stringify(basket));
+  }
 
   console.log(basket);
   return (
     <>
       <Navigation />
-      <h1>Kurv</h1>
-      {empty ? (
-        <h1>Du har ikke tilføjet til kurv endnu</h1>
-      ) : (
-        <>
-          <h1>Her er dine varer</h1>
-          <p>test</p>
-          {basket?.map((product) => {
-            const productFind = products.find((e) => e.id === product.product);
-            let pricetot = Math.floor(
-              parseFloat(product.qauntity) *
-                parseFloat(productFind?.data().productPrice)
-            );
-            console.log(pricetot);
-            // setTotalprice((prev) => prev + pricetot);
-            return (
-              <>
-                <p>{productFind?.data().productName}</p>
-                <div>
-                  <p>Antal:</p>
-                  <input type="number" defaultValue={product.qauntity} />
-                </div>
-                <p>Pris: {parseFloat(productFind?.data().productPrice)} dkk</p>
-                <img
-                  alt={productFind?.data().productName}
-                  src={productFind?.data().imagePath}
-                  width={"100px"}
-                />
-              </>
-            );
-          })}
-        </>
-      )}
-
-      {"totalpris:" + totalprice + "dkk"}
+      <div className="breadcrumb">
+        <Link className="webshop-crumb" to="/webshop">
+          Webshop <ArrowLeftIcon width="30px" />
+        </Link>
+        <p>Kurv</p>
+      </div>
+      <div className="basket-container">
+        <div className="product-column">
+          {empty ? (
+            <h1>Du har ikke tilføjet til kurv endnu</h1>
+          ) : (
+            <>
+              {basket?.map((product) => {
+                const productFind = products.find(
+                  (e) => e.id === product.product
+                );
+                let pricetot = Math.floor(
+                  parseFloat(product.qauntity) *
+                    parseFloat(productFind?.data().productPrice)
+                );
+                console.log(pricetot);
+                // setTotalprice((prev) => prev + pricetot);
+                return (
+                  <div className="product-container">
+                    <div className="image-container">
+                      <img
+                        alt={productFind?.data().productName}
+                        src={productFind?.data().imagePath}
+                        height={"205px"}
+                      />
+                    </div>
+                    <div>
+                      <TrashIcon
+                        onClick={(e) => removeItem(product.product)}
+                        width="40px"
+                        height="40px"
+                        fill="white"
+                      />
+                      <h4>{productFind?.data().productName}</h4>
+                      <h5>
+                        Pris: {parseFloat(productFind?.data().productPrice)} dkk
+                      </h5>
+                      <div className="number-of-product">
+                        <p>Antal: {product.qauntity}</p>
+                        <input
+                          type="number"
+                          onClick={(e) =>
+                            handleChangeInput(e, product?.product)
+                          }
+                          onChange={(e) =>
+                            handleChangeInput(e, product?.product)
+                          }
+                          defaultValue={product?.qauntity}
+                          min={1}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </>
+          )}
+        </div>
+        <div className="total-column">
+          <h2>Oversigt</h2>
+          <div className="total-price">
+            <h5>Subtotal</h5>
+            <h5> {totalprice + " DKK"}</h5>
+          </div>
+          <div className="delivery">
+            <p>Levering</p>
+            <p>Beregnes i checkout</p>
+          </div>
+          <div className="add-discount">
+            <label className="discount">
+              Rabatkode
+              <input type="text" />
+            </label>
+            <SecondaryButton text="Tilføj" />
+          </div>
+          <div className="delivery">
+            <h3>Total eksl. levering</h3>
+            <h3>{totalprice + " DKK"}</h3>
+          </div>
+          <p>Inkl. moms</p>
+          <PrimaryButton text="Checkout" />
+        </div>
+      </div>
     </>
   );
 }
