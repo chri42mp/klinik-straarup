18205ce661a05bbc87070fb8308205a03a4c50b3
chri42mp <99641832+chri42mp@users.noreply.github.com>
2023-12-12 14:36:39 +0100

    user dashboard + ignore branch name

--- src/pages/UserDashboard.js
+++ src/pages/UserDashboard.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { auth } from "../firebase";
+import { auth, database } from "../firebase";
 import Navigation from "../components/Navigation";
 import Dropdown from "../components/Dropdown";
 import { TertiaryButtonWithIcon } from "../components/Buttons";
@@ -8,18 +8,33 @@ import LongArrowRightIcon from "../assets/icons/LongArrowRightIcon";
 
 export default function UserDashboard() {
   const navigate = useNavigate();
+  const [currentUser, setCurrentUser] = useState();
 
   useEffect(() => {
     console.log(auth.currentUser.uid);
     if (auth.currentUser.uid === "uFq7ihsSmhPn3tc3NBsl72flECo1") {
       navigate("/addproduct");
     }
+    if (auth.currentUser.uid) {
+      database
+        .collection("users")
+        .doc(auth.currentUser.uid)
+        .get()
+        .then((data) => {
+          setCurrentUser(data);
+        });
+    }
   }, [navigate]);
 
+  console.log(currentUser?.data());
   return (
     <>
       <Navigation />
-      <h1>Hej, {}</h1>
+      <h1>
+        Hej{" "}
+        {currentUser?.data() &&
+          `${currentUser?.data()?.firstname} ${currentUser?.data()?.lastname}`}
+      </h1>
       <Dropdown title="Mine ordre" content={<p>jeg er en test</p>} />
       <Dropdown title="Mine tider" content={<p>jeg er en test</p>} />
       <Dropdown title="Mine favoritter" content={<p>jeg er en test</p>} />
