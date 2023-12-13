ff667746c93f02c6693da8e70841ea53aaa7bcc7
chri42mp <99641832+chri42mp@users.noreply.github.com>
2023-12-13 17:08:21 +0100

    classname instead of class

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './Loader.scss';
+import React from "react";
+import "./Loader.scss";
 
 const Loader = () => {
   return (
-    <div class='spinner'>
-      <div class='spinner__circle'>
-        <div class='spinner__circle-gradient'></div>
+    <div className="spinner">
+      <div className="spinner-circle">
+        <div className="spinner-circle-gradient"></div>
 
-        <div class='spinner__circle-inner'></div>
+        <div className="spinner-circle-inner"></div>
       </div>
     </div>
   );
