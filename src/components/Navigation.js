1e555b6b4b13ebe93ed80392264015139f391191
chri42mp <99641832+chri42mp@users.noreply.github.com>
2023-12-14 21:57:23 +0100

    navigation acting stupid

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -23,25 +23,71 @@ export default function Navigation() {
 
   return (
     <nav>
-      <Link to="/">
-        <img
-          className="logo-mobile"
-          src="/KS-logo-element_gold.png"
-          alt="klinik strarup logo"
-        />
-      </Link>
-      <Link to="/login">
-        <span className="visually-hidden">Gå til login</span>
-        <LoginIcon />
-      </Link>
-      <Link to="/basket">
-        <span className="visually-hidden">Gå til kurv</span>
-        <BasketIcon />
-      </Link>
-      <MenuIcon onClick={handleMobileMenuToggle} />
+      <div className="logo-container">
+        <Link to="/">
+          <img
+            className="logo-mobile"
+            src="/KS-logo-element_gold.png"
+            alt="klinik strarup logo"
+          />
+        </Link>
+      </div>
+      <div className="navigation-elements">
+        <Link to="/login">
+          <span className="visually-hidden">Gå til login</span>
+          <LoginIcon />
+        </Link>
+        <Link to="/basket">
+          <span className="visually-hidden">Gå til kurv</span>
+          <BasketIcon />
+        </Link>
+        <MenuIcon onClick={handleMobileMenuToggle} />
+      </div>
       {isMobileMenuVisible && (
         <ul className="mobile-menu">
-          <li
+          <ul onClick={handleSubmenuClicked}>
+            Behandlinger
+            <li>
+              <Link to="/fillers">Filler</Link>
+            </li>
+            <li>
+              <Link to="/botox">Botox</Link>
+            </li>
+            <li>
+              <Link to="/laser">Permanent hårfjerning</Link>
+            </li>
+            <li>
+              <Link to="/sclerosis">Sklerosering</Link>
+            </li>
+            <li>
+              <Link to="/skinbooster">Skinbooster</Link>
+            </li>
+          </ul>
+          <li>
+            <Link to="/prices">Priser</Link>
+          </li>
+          <li>
+            <Link to="/webshop">Webshop</Link>
+          </li>
+          <li>
+            <Link to="/about">Om</Link>
+          </li>
+          <li>
+            <Link to="/contact">Kontakt</Link>
+          </li>
+          <li>
+            <PrimaryButton
+              text="Book"
+              onClick={() => console.log("Knap klikket!")}
+            />
+          </li>
+        </ul>
+      )}
+    </nav>
+  );
+}
+
+/* <ul
             className={`treatments ${isSubmenuVisible ? "active" : ""}`}
             onClick={handleSubmenuClicked}
           >
@@ -70,27 +116,4 @@ export default function Navigation() {
                 </li>
               </ul>
             )}
-          </li>
-          <li>
-            <Link to="/prices">Priser</Link>
-          </li>
-          <li>
-            <Link to="/webshop">Webshop</Link>
-          </li>
-          <li>
-            <Link to="/about">Om</Link>
-          </li>
-          <li>
-            <Link to="/contact">Kontakt</Link>
-          </li>
-          <li>
-            <PrimaryButton
-              text="Book"
-              onClick={() => console.log("Knap klikket!")}
-            />
-          </li>
-        </ul>
-      )}
-    </nav>
-  );
-}
+          </ul> */
