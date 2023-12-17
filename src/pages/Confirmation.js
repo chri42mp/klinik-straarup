1e0d32920929bc6095750373d6e7d12d2fcf1f7b
bykamie89 <kamillakkn@gmail.com>
2023-12-17 14:44:10 +0100

    confirmation setup

--- src/pages/Confirmation.js
+++ src/pages/Confirmation.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
-import Navigation from "../components/Navigation";
-import { SecondaryButton } from "../components/Buttons";
-import DiscountBanner from "../components/DiscountBanner";
-import CustomFooter from "../components/CustomFooter";
-import Loader from "../components/Loader";
-import "./Confirmation.scss";
-import { Helmet } from "react-helmet";
+import React, { useEffect, useState } from 'react';
+import Navigation from '../components/Navigation';
+import { SecondaryButton } from '../components/Buttons';
+import DiscountBanner from '../components/DiscountBanner';
+import CustomFooter from '../components/CustomFooter';
+import Loader from '../components/Loader';
+import './Confirmation.scss';
+import { Helmet } from 'react-helmet';
 
 export default function Confirmation() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const delay = 2000000;
+    const delay = 2000;
 
     const timeoutId = setTimeout(() => {
       setLoading(false);
@@ -24,22 +24,32 @@ export default function Confirmation() {
     <>
       <Helmet>
         <title>Bekræftelse af køb</title>
-        <meta name="description" content="Denne side bekræfter et køb på webshoppen" />
+        <meta
+          name='description'
+          content='Denne side bekræfter et køb på webshoppen'
+        />
       </Helmet>
 
       <Navigation />
       {loading ? (
-        <div className="delay">
+        <div className='delay'>
           <Loader />
           <p>Du sendes nu videre til sikker betaling</p>
         </div>
       ) : (
         <>
-          <h1>Tak for din ordre!</h1>
-          <h5>Vi glæder os til at sende den!</h5>
-          <p>Vi har sendt dig en bekræftelse på mail og sender ordren afsted hurtigst muligt. Tak fordi du handlede hos os! </p>
-          <p>Har du set at vi også tilbyder skønhedsbehandlinger?</p>
-          <SecondaryButton text="Besøg klinikken" />
+          <div className='confirmation-textbox'>
+            <h1>Tak for din ordre!</h1>
+            <h5>Vi glæder os til at sende den!</h5>
+            <p>
+              Vi har sendt dig en bekræftelse på mail og sender ordren afsted
+              hurtigst muligt. Tak fordi du handlede hos os!{' '}
+            </p>
+            <p>Har du set at vi også tilbyder skønhedsbehandlinger?</p>
+            <div className='confirmation-btn'>
+              <SecondaryButton text='Besøg klinikken' />
+            </div>
+          </div>
           <DiscountBanner />
         </>
       )}
