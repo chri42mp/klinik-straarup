eb8f46ef09038779907f3661d5839d0f4938287e
bykamie89 <kamillakkn@gmail.com>
2023-12-17 22:26:21 +0100

    fixing max width, done on these elements

--- src/components/DiscountBanner.js
+++ src/components/DiscountBanner.js
@@ -1,20 +1,24 @@
-import React from "react";
-import "./DiscountBanner.scss";
-import { SecondaryButton } from "./Buttons";
+import React from 'react';
+import './DiscountBanner.scss';
+import { SecondaryButton } from './Buttons';
 
 const DiscountBanner = () => {
   return (
-    <div className="discount-banner">
-      <div className="discount-banner-text">
-        <h2>10% rabat</h2>
-        <p>
-          Som loyalitetskunde hos Klinik Straarup modtager du 10% på hver
-          behandling efter den første. Opret en kundeprofil hos os og bestil en
-          gratis tid til konsultation i dag.
-        </p>
-      </div>
-      <div>
-        <SecondaryButton text="Opret kundeprofil" link={"/login"} />
+    <div className='discount-banner-background'>
+      <div className='max-content'>
+        <div className='discount-banner '>
+          <div className='discount-banner-text'>
+            <h2>10% rabat</h2>
+            <p>
+              Som loyalitetskunde hos Klinik Straarup modtager du 10% på hver
+              behandling efter den første. Opret en kundeprofil hos os og bestil
+              en gratis tid til konsultation i dag.
+            </p>
+          </div>
+          <div>
+            <SecondaryButton text='Opret kundeprofil' link={'/login'} />
+          </div>
+        </div>
       </div>
     </div>
   );
