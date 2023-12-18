737c158a29fc0433b5cfe73054cab16a40dd1fa5
bykamie89 <kamillakkn@gmail.com>
2023-12-18 08:46:20 +0100

    shipping responsive

--- src/pages/Shipping.js
+++ src/pages/Shipping.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
-import Navigation from "../components/Navigation";
-import { Link, useNavigate } from "react-router-dom";
-import ArrowLeftIcon from "../assets/icons/ArrowLeftIcon";
-import { PrimaryButton, SecondaryButton } from "../components/Buttons";
-import { auth, database } from "../firebase";
-import "./Shipping.scss";
-import DiscountBanner from "../components/DiscountBanner";
-import CustomFooter from "../components/CustomFooter";
-import { Helmet } from "react-helmet";
-import { createOrderNumber } from "../components/CreateOrderNumber";
+import React, { useEffect, useState } from 'react';
+import Navigation from '../components/Navigation';
+import { Link, useNavigate } from 'react-router-dom';
+import ArrowLeftIcon from '../assets/icons/ArrowLeftIcon';
+import { PrimaryButton, SecondaryButton } from '../components/Buttons';
+import { auth, database } from '../firebase';
+import './Shipping.scss';
+import DiscountBanner from '../components/DiscountBanner';
+import CustomFooter from '../components/CustomFooter';
+import { Helmet } from 'react-helmet';
+import { createOrderNumber } from '../components/CreateOrderNumber';
 
 export default function Shipping() {
   const [finalBasket, setFinalBasket] = useState({});
@@ -19,12 +19,12 @@ export default function Shipping() {
   const navigate = useNavigate();
 
   const userFields = [
-    "firstname",
-    "lastname",
-    "phone",
-    "adress",
-    "zipcode",
-    "city",
+    'firstname',
+    'lastname',
+    'phone',
+    'adress',
+    'zipcode',
+    'city',
   ];
 
   const handleDeliveryOptionChange = (option) => {
@@ -36,10 +36,10 @@ export default function Shipping() {
   };
 
   useEffect(() => {
-    setFinalBasket(JSON.parse(localStorage.getItem("finalBasket")));
+    setFinalBasket(JSON.parse(localStorage.getItem('finalBasket')));
 
     database
-      .collection("products")
+      .collection('products')
       .get()
       .then((data) => {
         setProducts(data.docs);
@@ -47,9 +47,9 @@ export default function Shipping() {
   }, []);
 
   useEffect(() => {
-    if (selectedDeliveryOption === "standard") {
+    if (selectedDeliveryOption === 'standard') {
       setDeliveryCost(39);
-    } else if (selectedDeliveryOption === "express") {
+    } else if (selectedDeliveryOption === 'express') {
       setDeliveryCost(79);
     } else {
       setDeliveryCost(0);
@@ -95,16 +95,16 @@ export default function Shipping() {
       date: new Date().toLocaleDateString(),
     };
 
-    console.log("orderData:", orderData);
+    console.log('orderData:', orderData);
 
     try {
       await database
-        .collection("orders")
-        .doc(createOrderNumber() + "#" + auth?.currentUser?.uid || "NoUser")
+        .collection('orders')
+        .doc(createOrderNumber() + '#' + auth?.currentUser?.uid || 'NoUser')
         .set(orderData);
-      navigate("/confirmation");
+      navigate('/confirmation');
     } catch (error) {
-      console.error("error creating order", error);
+      console.error('error creating order', error);
     }
   };
 
@@ -113,135 +113,140 @@ export default function Shipping() {
       <Helmet>
         <title>Leveringsmuligheder</title>
         <meta
-          name="description"
-          content="Denne side bruges til at udfylde leveringsmuligheder"
+          name='description'
+          content='Denne side bruges til at udfylde leveringsmuligheder'
         />
       </Helmet>
       <Navigation />
-      <div className="breadcrumb">
-        <Link className="webshop-crumb" to="/webshop">
-          Webshop <ArrowLeftIcon width="30px" />
-        </Link>
-        <Link className="webshop-crumb" to="/basket">
-          Kurv <ArrowLeftIcon width="30px" />
-        </Link>
-        <Link className="webshop-crumb" to="/checkout">
-          Kontaktoplysninger <ArrowLeftIcon width="30px" />
-        </Link>
-        <p className="active-page">Checkout</p>
-      </div>
-      <div className="grid-1-1">
-        <div>
-          <div className="delivery-container">
-            <h5>Leveringsmuligheder</h5>
-            <div className="delivery-options">
-              <label>
-                <input
-                  type="checkbox"
-                  checked={selectedDeliveryOption === "standard"}
-                  onChange={() => handleDeliveryOptionChange("standard")}
-                />
-                Standard
-              </label>
-              <span>3-5 dage</span>
-            </div>
-            <div className="delivery-options">
-              <label>
-                <input
-                  type="checkbox"
-                  checked={selectedDeliveryOption === "express"}
-                  onChange={() => handleDeliveryOptionChange("express")}
-                />
-                Express
-              </label>
-              <span>1-2 dage</span>
-            </div>
-          </div>
-          <form>
-            <h5>Vælg betalingsmulighed</h5>
-            <div className="payment-option">
-              <input
-                type="radio"
-                id="creditCard"
-                name="payment"
-                value="creditCard"
-                checked={selectedPayment === "creditCard"}
-                onChange={() => handlePaymentChange("creditCard")}
-              />
-              <label htmlFor="creditCard"> Betalingskort</label>
-              <div className="image-container">
-                <img src="/site-images/visa.png" alt="Betalingskort" />
-                <img src="/site-images/mastercard.png" alt="Betalingskort" />
-              </div>
-            </div>
-            <div className="payment-option">
-              <input
-                type="radio"
-                id="klarna"
-                name="klarna"
-                value="klarna"
-                checked={selectedPayment === "klarna"}
-                onChange={() => handlePaymentChange("klarna")}
-              />
-              <label htmlFor="klarna">Klarna</label>
-              <div className="image-container">
-                <img src="/site-images/klarna.png" alt="klarna" />
-              </div>
-            </div>
-          </form>
-          <div className="safe-payment-btn">
-            <PrimaryButton
-              text="Gå til sikker betaling"
-              onClick={handleSecurePayment}
-            />
-          </div>
-          <div className="terms-links">
-            <Link to="/tradeconditions">Handelsbetingelser</Link>
-          </div>
+      <div className='max-content'>
+        <div className='breadcrumb'>
+          <Link className='webshop-crumb' to='/webshop'>
+            Webshop <ArrowLeftIcon width='30px' />
+          </Link>
+          <Link className='webshop-crumb' to='/basket'>
+            Kurv <ArrowLeftIcon width='30px' />
+          </Link>
+          <Link className='webshop-crumb' to='/checkout'>
+            Kontaktoplysninger <ArrowLeftIcon width='30px' />
+          </Link>
+          <p className='active-page'>Checkout</p>
         </div>
-
-        <div className="column">
-          <div>
-            {finalBasket?.basket?.map((item) => {
-              const product = products
-                ?.find((e) => e.id === item.product)
-                ?.data();
-              console.log(product);
-              return (
-                <div className="basket-products">
-                  <div className="image-container">
-                    <img alt={product?.productName} src={product?.imagePath} />
-                  </div>
-                  <div className="space-between">
-                    <p>{product?.productName}</p>
-                    <p>{product?.productPrice + " DKK"}</p>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          <div className="total-column">
-            <h2>Oversigt</h2>
-            <div className="total-price">
-              <h5>Subtotal</h5>
-              <h5> {finalBasket.totalprice + " DKK"}</h5>
-            </div>
-            <div className="delivery">
-              <h5>Levering</h5>
-              <h5>{deliveryCost.toFixed(2)} DKK</h5>
-            </div>
-            <div className="add-discount">
-              <div className="discount-group">
-                <label for="discount">Rabatkode</label>
-                <input type="text" id="discount" name="discount" />
+        <div className='grid-1-1'>
+          <div className='delivery-container-width'>
+            <div className='delivery-container'>
+              <h5>Leveringsmuligheder</h5>
+              <div className='delivery-options'>
+                <label>
+                  <input
+                    type='checkbox'
+                    checked={selectedDeliveryOption === 'standard'}
+                    onChange={() => handleDeliveryOptionChange('standard')}
+                  />
+                  Standard
+                </label>
+                <span>3-5 dage</span>
               </div>
-              <SecondaryButton text="Tilføj" link={"/shipping"} />
+              <div className='delivery-options'>
+                <label>
+                  <input
+                    type='checkbox'
+                    checked={selectedDeliveryOption === 'express'}
+                    onChange={() => handleDeliveryOptionChange('express')}
+                  />
+                  Express
+                </label>
+                <span>1-2 dage</span>
+              </div>
             </div>
-            <div className="delivery">
-              <h3>Total inkl. levering</h3>
-              <h3>{finalBasket.totalprice + " DKK"}</h3>
+            <form className='payment-form'>
+              <h5>Vælg betalingsmulighed</h5>
+              <div className='payment-option'>
+                <input
+                  type='radio'
+                  id='creditCard'
+                  name='payment'
+                  value='creditCard'
+                  checked={selectedPayment === 'creditCard'}
+                  onChange={() => handlePaymentChange('creditCard')}
+                />
+                <label htmlFor='creditCard'> Betalingskort</label>
+                <div className='image-container'>
+                  <img src='/site-images/visa.png' alt='Betalingskort' />
+                  <img src='/site-images/mastercard.png' alt='Betalingskort' />
+                </div>
+              </div>
+              <div className='payment-option'>
+                <input
+                  type='radio'
+                  id='klarna'
+                  name='klarna'
+                  value='klarna'
+                  checked={selectedPayment === 'klarna'}
+                  onChange={() => handlePaymentChange('klarna')}
+                />
+                <label htmlFor='klarna'>Klarna</label>
+                <div className='image-container'>
+                  <img src='/site-images/klarna.png' alt='klarna' />
+                </div>
+              </div>
+            </form>
+            <div className='safe-payment-btn'>
+              <PrimaryButton
+                text='Gå til sikker betaling'
+                onClick={handleSecurePayment}
+              />
             </div>
-            <p>Inkl. moms</p>
+            <div className='shipping-terms-links'>
+              <Link to='/tradeconditions'>Handelsbetingelser</Link>
+            </div>
+          </div>
+
+          <div className='column'>
+            <div className='shipping-basket-products'>
+              {finalBasket?.basket?.map((item) => {
+                const product = products
+                  ?.find((e) => e.id === item.product)
+                  ?.data();
+                console.log(product);
+                return (
+                  <div className='shipping-basket-item'>
+                    <div className='shipping-basket-image-container'>
+                      <img
+                        alt={product?.productName}
+                        src={product?.imagePath}
+                      />
+                    </div>
+                    <div className='shipping-basket-product-info'>
+                      <p>{product?.productName}</p>
+                      <p>{product?.productPrice + ' DKK'}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className='total-column'>
+              <h2>Oversigt</h2>
+              <div className='total-price'>
+                <h5>Subtotal</h5>
+                <h5> {finalBasket.totalprice + ' DKK'}</h5>
+              </div>
+              <div className='delivery'>
+                <h5>Levering</h5>
+                <h5>{deliveryCost.toFixed(2)} DKK</h5>
+              </div>
+              <div className='add-discount'>
+                <div className='discount-group'>
+                  <label for='discount'>Rabatkode</label>
+                  <input type='text' id='discount' name='discount' />
+                </div>
+                <SecondaryButton text='Tilføj' link={'/shipping'} />
+              </div>
+              <div className='delivery'>
+                <h3>Total inkl. levering</h3>
+                <h3>{finalBasket.totalprice + ' DKK'}</h3>
+              </div>
+              <p>Inkl. moms</p>
+            </div>
           </div>
         </div>
       </div>
