757fa0b0da3e95efa926eeaa3c60093296774a04
bykamie89 <kamillakkn@gmail.com>
2023-12-18 03:26:56 +0100

    fixed webshop frontpage

---
+++ src/components/ProductTextBox.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TertiaryButtonWithIcon } from './Buttons';
+import './ProductTextBox.scss';
+
+const ProductTextBox = ({
+  title,
+  text,
+  imageUrl,
+  alt,
+  buttonText,
+  buttonIcon,
+  onButtonClick,
+  reverse,
+}) => {
+  return (
+    <div className='max-content'>
+      <div className={`banner ${reverse ? 'reverse' : ''}`}>
+        <div className='banner-image' style={{ order: reverse }}>
+          <img src={imageUrl} alt={alt} />
+        </div>
+        <div className='banner-box' style={{ order: reverse }}>
+          <h2>{title}</h2>
+          <p>{text}</p>
+          <div>
+            <TertiaryButtonWithIcon
+              text={buttonText}
+              onClick={onButtonClick}
+              icon={buttonIcon}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ProductTextBox.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string,
+  imageUrl: PropTypes.string,
+  alt: PropTypes.string,
+  buttonText: PropTypes.string,
+  buttonIcon: PropTypes.element,
+  onButtonClick: PropTypes.func,
+  reverse: PropTypes.bool,
+};
+
+export default ProductTextBox;
